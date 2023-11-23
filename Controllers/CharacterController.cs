bf439effdec24900393c7bb2bf55a0708feb0fee
BhagyaMS <Bhagya.M.S@gds.ey.com>
2023-11-24 00:36:19 +0530

    Added Services folder

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private readonly iCharacterService _characterService;
+
+        public CharacterController(iCharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get() {
+            return Ok(_characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id) {
+            return Ok(_characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter) {
+            return Ok(_characterService.AddCharacter(newCharacter));
+        }
+    }
+}
